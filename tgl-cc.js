c4e652d75db5c51b0240829f4387eec6319ceb45
Tzero <56503208+Tzero-real@users.noreply.github.com>
2022-03-05 22:29:47 +0800

    Update tgl-cc.js

--- tgl-cc.js
+++ tgl-cc.js
@@ -1,4 +1,4 @@
-// components/tgl-cc/tgl-cc.js
+// 本组件由TGL工作室提供
 Component({
   /**
    * 组件的属性列表
@@ -11,13 +11,21 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    nowTime:'2022'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    
+  },
+  lifetimes: {
+    attached: function() {
+        this.setData({
+          nowTime:new Date().getFullYear()
+        })    
+    }
+  },
 
-  }
 })
